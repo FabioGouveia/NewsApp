d4fc90e718baa7830fc7e5e7712919808df69d09
Fábio Gouveia <hpgroods@gmail.com>
2018-06-10 17:48:21 +0100

    fix : remove ViewHolder inner class
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/newsapp/core/NewsListAdapter.java
+++ app/src/main/java/com/example/android/newsapp/core/NewsListAdapter.java
@@ -2,14 +2,12 @@ package com.example.android.newsapp.core;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.example.android.newsapp.R;
 import com.example.android.newsapp.model.NewsItem;
@@ -17,7 +15,7 @@ import com.example.android.newsapp.model.NewsItem;
 import java.util.ArrayList;
 import java.util.List;
 
-public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHolder> {
+public class NewsListAdapter extends RecyclerView.Adapter<NewsItemViewHolder> {
 
     private Context context;
     private List<NewsItem> newsList;
@@ -34,30 +32,30 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
 
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+    public NewsItemViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         View view = LayoutInflater.from(context).inflate(R.layout.card_news_item, parent, false);
-        return new ViewHolder(view);
+        return new NewsItemViewHolder(view);
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull NewsItemViewHolder holder, int position) {
 
         final NewsItem newsItem = newsList.get(position);
 
-        holder.section.setText(newsItem.getSection());
-        holder.title.setText(newsItem.getTitle());
+        holder.sectionTextView.setText(newsItem.getSection());
+        holder.titleTextView.setText(newsItem.getTitle());
 
         String author = newsItem.getAuthor();
         String date = newsItem.getDate();
 
         if(author != null){
-            holder.author.setVisibility(View.VISIBLE);
-            holder.author.setText(author);
+            holder.authorTextView.setVisibility(View.VISIBLE);
+            holder.authorTextView.setText(author);
         }
 
         if(date != null){
-            holder.date.setVisibility(View.VISIBLE);
-            holder.date.setText(date);
+            holder.dateTextView.setVisibility(View.VISIBLE);
+            holder.dateTextView.setText(date);
         }
 
         holder.itemView.setOnClickListener(new View.OnClickListener() {
@@ -72,22 +70,5 @@ public class NewsListAdapter extends RecyclerView.Adapter<NewsListAdapter.ViewHo
     @Override
     public int getItemCount() {
         return newsList.size();
-    }
-
-    static class ViewHolder extends RecyclerView.ViewHolder{
-
-        private TextView section;
-        private TextView title;
-        private TextView author;
-        private TextView date;
-
-        private ViewHolder(View itemView) {
-            super(itemView);
-
-            section = itemView.findViewById(R.id.news_item_section);
-            title = itemView.findViewById(R.id.news_item_title);
-            author = itemView.findViewById(R.id.news_item_author);
-            date = itemView.findViewById(R.id.news_item_date);
-        }
     }
 }
