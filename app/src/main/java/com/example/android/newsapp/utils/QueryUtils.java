2c781b9df00b57d63c7fb530a638966bc67132b8
Fábio Gouveia <hpgroods@gmail.com>
2018-06-13 08:50:12 +0100

    fix : change getNews method declaration
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/newsapp/utils/QueryUtils.java
+++ app/src/main/java/com/example/android/newsapp/utils/QueryUtils.java
@@ -1,12 +1,14 @@
 package com.example.android.newsapp.utils;
 
+import android.content.Context;
+
 import com.example.android.newsapp.model.NewsItem;
 
 import java.util.List;
 
 public class QueryUtils {
 
-    public static List<NewsItem> getNews() {
-        return JsonUtils.extractNews(HttpUtils.requestNews());
+    public static List<NewsItem> getNews(Context context) {
+        return JsonUtils.extractNews(HttpUtils.requestNews(context));
     }
 }
